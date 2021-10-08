05eee4e23a370a4983a7c11060f76d8a6800129e
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-10-08 14:13:19 -0700

    Update mygemm.c

--- mygemm.c
+++ mygemm.c
@@ -310,24 +310,11 @@ void bkji(const double *A, const double *B, double *C, const int n, const int b)
 //Cache Reuse part 4
 void optimal(const double* A, const double* B, double *C, const int n, const int b)
 {
-    /* ijk – simple triple loop algorithm with simple single register reuse*/
-    for (i=0; i<n; i++)
     for (j=0; j<n; j++) {
-        register double r=c[i*n+j];
-        for (k=0; k<n; k++)
-        r += a[i*n+k] * b[k*n+j];
-        c[i*n+j]=r;
+        for (k=0; k<n; k++) {
+            r = b[k][j];
+            for (i=0; i<n; i++)
+                c[i][j] += a[i][k] * r;
         }
-        /* ijk – blocked version algorithm*/
-        for (i = 0; i < n; i+=B)
-        for (j = 0; j < n; j+=B)
-        for (k = 0; k < n; k+=B)
-        /* B x B mini matrix multiplications */
-        for (i1 = i; i1 < i+B; i1++)
-        for (j1 = j; j1 < j+B; j1++) {
-            register double r=c[i1*n+j1];
-            for (k1 = k; k1 < k+B; k1++)
-            r += a[i1*n + k1]*b[k1*n + j1];
-            c[i1*n+j1]=r;
-            }
+    }
 }
